2d4fe338263206ebf88c69357479857c72c7a2c8
Takaki Takeuchi <takaki@takeu.ch>
2022-09-11 20:25:10 +0900

    feat: implement startPresale function

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.9;
 
+import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
@@ -10,13 +11,23 @@ import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
 import 'hardhat/console.sol';
 
 contract Presale is Ownable, ERC165 {
+    using Counters for Counters.Counter;
+
+    /**
+     * Constants
+     */
+
+    uint16 constant FEE_AMOUNT_PRECISION = 10000;
+
     /**
      * Variables
      */
 
     uint256 public feeAmount;
 
-    struct TokenSale {
+    Counters.Counter private _saleIds;
+
+    struct Sale {
         uint256 startTimestamp;
         uint256 endTimestamp;
         uint256 price;
@@ -25,8 +36,8 @@ contract Presale is Ownable, ERC165 {
         address creatorAddress;
     }
 
-    // Array with all token sales
-    TokenSale[] public allTokenSales;
+    // mapping from saleId to Sale
+    mapping(uint256 => Sale) public sales;
 
     /**
      * Events
@@ -50,10 +61,6 @@ contract Presale is Ownable, ERC165 {
      * Public functions
      */
 
-    function totalTokenSales() public view returns (uint256) {
-        return allTokenSales.length;
-    }
-
     function startPresale(
         uint256[] memory _startTimestamps,
         uint256[] memory _endTimestamps,
@@ -63,7 +70,8 @@ contract Presale is Ownable, ERC165 {
     ) public {
         // check if the arrays have the same length
         require(
-            _startTimestamps.length == _endTimestamps.length &&
+            _startTimestamps.length > 0 &&
+                _startTimestamps.length == _endTimestamps.length &&
                 _startTimestamps.length == _prices.length &&
                 _startTimestamps.length == _tokenAmounts.length &&
                 _startTimestamps.length == _tokenAddresses.length,
@@ -99,7 +107,7 @@ contract Presale is Ownable, ERC165 {
 
     function setFeeAmount(uint256 _feeAmount) public onlyOwner {
         // Check if new fee amount is in valid range
-        require(_feeAmount <= 10000, 'Invalid Fee Amount');
+        require(_feeAmount <= FEE_AMOUNT_PRECISION, 'Invalid Fee Amount');
         feeAmount = _feeAmount;
         emit FeeAmountChanged(feeAmount);
     }
@@ -118,16 +126,28 @@ contract Presale is Ownable, ERC165 {
     ) internal returns (uint256) {
         require(_tokenAddress != address(0), 'Token address is zero address');
 
-        // check if the token contract have enough tokens to sell
+        // check if endTimestamp is after startTimestamp
         require(
-            IERC20(_tokenAddress).balanceOf(_creatorAddress) >= _tokenAmount,
-            'Token address does not have enough tokens to sell'
+            _endTimestamp > _startTimestamp,
+            'endTimestamp must be after startTimestamp'
+        );
+
+        // issue new sale id
+        _saleIds.increment();
+        uint256 saleId = _saleIds.current();
+
+        console.log('saleId', saleId);
+
+        // transfer tokens to this contract
+        IERC20 tokenContract = IERC20(_tokenAddress);
+        tokenContract.transferFrom(
+            _creatorAddress,
+            address(this),
+            _tokenAmount
         );
 
         // create new presale
-        uint256 newSaleId = totalTokenSales();
-
-        allTokenSales[newSaleId] = TokenSale(
+        sales[saleId] = Sale(
             _startTimestamp,
             _endTimestamp,
             _price,
@@ -137,9 +157,9 @@ contract Presale is Ownable, ERC165 {
         );
 
         // emit event
-        emit SaleCreated(newSaleId);
+        emit SaleCreated(saleId);
 
         // return created sale id
-        return newSaleId;
+        return saleId;
     }
 }
--- test/Presale.test.ts
+++ test/Presale.test.ts
@@ -1,42 +1,56 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import { BigNumber } from 'ethers';
 
 describe('Presale', function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshopt in every test.
+
+  // Fixture for deploying MockToken contract
+  async function deployMockTokenContractFixture() {
+    const [owner, otherAccount] = await ethers.getSigners();
+    const MockToken = await ethers.getContractFactory('MockToken');
+    const mockToken = await MockToken.deploy();
+    return { mockToken, owner, otherAccount };
+  }
+
+  // Fixture for deploying Presale contract
   async function deployPresaleContractFixture() {
     const FEE_AMOUNT = 500; // 5% in basis points
-
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
-
     const Presale = await ethers.getContractFactory('Presale');
     const presale = await Presale.deploy(FEE_AMOUNT, owner.address);
-
     return { presale, owner, otherAccount };
   }
 
-  describe('Deployment', function () {
-    it('Should set the right owner', async function () {
+  /**
+   * constructor
+   */
+
+  describe('constructor', function () {
+    it('should set the right owner', async function () {
       const { presale, owner } = await loadFixture(
         deployPresaleContractFixture
       );
-
       expect(await presale.owner()).to.equal(owner.address);
     });
 
-    it('Should set the initial fee amount', async function () {
+    it('should set the initial fee amount', async function () {
       const { presale } = await loadFixture(deployPresaleContractFixture);
-
       expect(await presale.feeAmount()).to.equal(500);
     });
   });
 
-  describe('SetFeeAmount', function () {
+  /**
+   * setFeeAmount
+   */
+
+  describe('setFeeAmount', function () {
     describe('Validations', function () {
-      it('Should not allow non-owner to set fee amount', async function () {
+      it('should not allow non-owner to set fee amount', async function () {
         const { presale, otherAccount } = await loadFixture(
           deployPresaleContractFixture
         );
@@ -46,7 +60,7 @@ describe('Presale', function () {
         ).to.be.revertedWith('Ownable: caller is not the owner');
       });
 
-      it('Should set allow owner to set fee amount', async function () {
+      it('should set allow owner to set fee amount', async function () {
         const { presale, owner } = await loadFixture(
           deployPresaleContractFixture
         );
@@ -55,7 +69,7 @@ describe('Presale', function () {
         await expect(await presale.feeAmount()).to.be.equal(800);
       });
 
-      it('Should reject invalid fee amount', async function () {
+      it('should reject invalid fee amount', async function () {
         const { presale, owner } = await loadFixture(
           deployPresaleContractFixture
         );
@@ -73,12 +87,115 @@ describe('Presale', function () {
     });
 
     describe('Events', function () {
-      it('Should emit an event on fee amount change', async function () {
+      it('should emit an event on fee amount change', async function () {
         const { presale } = await loadFixture(deployPresaleContractFixture);
 
         await expect(presale.setFeeAmount(800))
           .to.emit(presale, 'FeeAmountChanged')
           .withArgs(800);
+      });
+    });
+  });
+
+  /**
+   * startPresale
+   */
+
+  describe.only('startPresale', () => {
+    describe('Validations', () => {
+      it('should not accept invalid length parameters', async () => {
+        const { presale } = await loadFixture(deployPresaleContractFixture);
+        await expect(
+          presale.startPresale([], [], [], [], [])
+        ).to.be.revertedWith('Invalid Argument Length');
+        await expect(
+          presale.startPresale([1663648034], [], [], [], [])
+        ).to.be.revertedWith('Invalid Argument Length');
+      });
+    });
+
+    describe('Happy path', () => {
+      it('should create single sale', async () => {
+        const { mockToken } = await loadFixture(deployMockTokenContractFixture);
+        // Contracts are deployed using the first signer/account by default
+        const [owner] = await ethers.getSigners();
+        const Presale = await ethers.getContractFactory('Presale');
+        const presale = await Presale.deploy(500, owner.address);
+
+        // 0.01 ETH ( Sell 1 MOK for 0.01 ETH)
+        const priceOfMok = BigNumber.from('10000000000000000');
+        const tokenAmount = BigNumber.from('100');
+
+        // approve presale contract to transfer MOK tokens
+        await mockToken.approve(presale.address, tokenAmount);
+
+        await expect(
+          presale.startPresale(
+            [1661002034],
+            [1663648034],
+            [priceOfMok],
+            [tokenAmount],
+            [mockToken.address]
+          )
+        )
+          .to.emit(presale, 'SaleCreated')
+          .withArgs(1);
+
+        // check if sale data is correct
+        const sale = await presale.sales(1);
+        expect(sale.startTimestamp).to.be.equal(1661002034);
+        expect(sale.endTimestamp).to.be.equal(1663648034);
+        expect(sale.price).to.be.equal(priceOfMok);
+        expect(sale.amount).to.be.equal(100);
+        expect(sale.tokenAddress).to.be.equal(mockToken.address);
+        expect(sale.creatorAddress).to.be.equal(owner.address);
+      });
+
+      it('should create multiple sales', async () => {
+        // deploy two MockToken contracts
+        // because fixture does not currently support multiple loads
+        const MockToken = await ethers.getContractFactory('MockToken');
+        const mockToken1 = await MockToken.deploy();
+        const mockToken2 = await MockToken.deploy();
+
+        // Contracts are deployed using the first signer/account by default
+        const [owner] = await ethers.getSigners();
+        const Presale = await ethers.getContractFactory('Presale');
+        const presale = await Presale.deploy(500, owner.address);
+
+        const priceOfMok1 = BigNumber.from('10000000000000000');
+        const priceOfMok2 = BigNumber.from('20000000000000000');
+
+        const tokenAmountOfMok1 = BigNumber.from('100');
+        const tokenAmountOfMok2 = BigNumber.from('200');
+
+        // set allowance for presale contract
+        await mockToken1.approve(presale.address, tokenAmountOfMok1);
+        await mockToken2.approve(presale.address, tokenAmountOfMok2);
+
+        await expect(
+          presale.startPresale(
+            [1661002034, 1661002034],
+            [1663648034, 1663648034],
+            [priceOfMok1, priceOfMok2],
+            [tokenAmountOfMok1, tokenAmountOfMok2],
+            [mockToken1.address, mockToken2.address]
+          )
+        ).not.to.be.reverted;
+
+        const firstSale = await presale.sales(1);
+        expect(firstSale.price).to.be.equal(priceOfMok1);
+
+        const secondSale = await presale.sales(2);
+        expect(secondSale.price).to.be.equal(priceOfMok2);
+
+        // confirm that the presale contract has the correct balance of MOK tokens
+        expect(await mockToken1.balanceOf(presale.address)).to.be.equal(
+          tokenAmountOfMok1
+        );
+        expect(await mockToken2.balanceOf(presale.address)).to.be.equal(
+          tokenAmountOfMok2
+        );
       });
     });
   });
